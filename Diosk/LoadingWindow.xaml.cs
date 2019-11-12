c0bda428e3bfb9bd05a340999bf7a70ab7405772
추서준 <chuseojun@naver.com>
2019-11-12 13:24:57 +0900

    [Fix] LoadingWindow
    1. LoadingWindow를 UserControl에서 Window로 변경하였습니다.
    2. Fade 처리를 하기 힘들어서 빼놨습니다,,

--- Diosk/LoadingWindow.xaml.cs
+++ Diosk/LoadingWindow.xaml.cs
@@ -11,7 +11,6 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Windows.Threading;
 
@@ -20,44 +19,62 @@ namespace Diosk
     /// <summary>
     /// Interaction logic for LoadingWindow.xaml
     /// </summary>
-    public partial class LoadingWindow : UserControl
+    public partial class LoadingWindow : Window
     {
         DispatcherTimer timer = new DispatcherTimer();
+
+        Diosk.Model.Menu menu = new Diosk.Model.Menu();
+
         public LoadingWindow()
         {
+            InitializeComponent();
             this.Loaded += LoadingWindow_Loaded;
         }
         private void LoadingWindow_Loaded(object sender, RoutedEventArgs e)
         {
+            alignCenter();
+            menu.Load();
             loadingStart();
         }
-        public void loadingStart()
+        private void alignCenter()
         {
-            InitializeComponent();
-            timer.Interval = new TimeSpan(0, 0, 1); // TimeSpan(Hour, Minute, Second);
+            Window currentWindow = Window.GetWindow(this);
+
+            currentWindow.Top = this.Top + (this.ActualHeight - currentWindow.Height) / 2;
+            currentWindow.Left = this.Left + (this.ActualWidth - currentWindow.Width) / 2;
+        }
+        private void loadingStart()
+        {
+            //timer.Interval = new TimeSpan(0, 0, 3); // TimeSpan(Hour, Minute, Second);
             timer.Tick += new EventHandler(timer_Tick);
-            
+
             timer.Start();
         }
         private void timer_Tick(object sender, EventArgs e)
         {
-            fadeOutAnimation(loading);
-            timer.Stop();
+            //fadeOutAnimation(loading);
+            //fadeOutAnimation(Window.GetWindow(this));
+            if(menu.isLoaded)
+            {
+                timer.Stop();
+                loadingClose();
+            }
         }
         private void loadingClose()
         {
+            Window main = new MainWindow();
             Window.GetWindow(this).Close();
+            main.Show();
         }
-        private void fadeOutAnimation(UIElement target)
+        /*private void fadeOutAnimation(Window target)
         {
-            DoubleAnimation anim = new DoubleAnimation();
-            double animRunningTime = 1.5f;
-            anim.From = 1.0;
-            anim.To = 0.0;
-            anim.Duration = new Duration(TimeSpan.FromSeconds(animRunningTime));
+                DoubleAnimation anim = new DoubleAnimation();
+                double animRunningTime = 1.5f;
+                anim.From = 1.0;
+                anim.To = 0.0;
+                anim.Duration = new Duration(TimeSpan.FromSeconds(animRunningTime));
 
-            target.BeginAnimation(OpacityProperty, anim);
-        }
+                target.BeginAnimation(OpacityProperty, anim);
+        }*/
     }
 }
-
